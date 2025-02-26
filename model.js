5028d717ec4e5b1d2863458ef8539712bbb5700b
Dean Gumas <dean.gumas25@gmail.com>
2025-02-25 22:29:09 -0800

    Added team data JSON files and updated model logic

--- model.js
+++ model.js
@@ -7,7 +7,9 @@ async function runModel() {
         const x_input = new ort.Tensor("float32", new Float32Array(6 * 10).fill(1.0), [1, 6, 10]); // Batch size 1
         const d_input = new ort.Tensor("float32", new Float32Array([0.5]), [1, 1]); // Batch size 1
 
-        const feeds = { "player_data": x_input, "team_rating": d_input };
+        const gk_data = await fetch("https://deangumas.github.io/data/GK_2021-22.json");
+        const sample = gk_data["David de Gea"]
+        const feeds = { "player_data": sample["player_data"], "team_rating": sample["team_rating"][0] };
         const results = await session.run(feeds);
 
         console.log(results["score_prediction"].data);
