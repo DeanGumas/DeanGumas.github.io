601a2cf770f948bb6d4796d12449fc2555e8d3f3
Dean Gumas <dean.gumas25@gmail.com>
2025-02-25 22:41:44 -0800

    Updated to sample randomly from goalkeeper datafile

--- model.js
+++ model.js
@@ -2,20 +2,25 @@ async function runModel() {
     try {
         // Load the ONNX model
         const session = await ort.InferenceSession.create("https://deangumas.github.io/model.onnx");
-
-        // Example input data (ensure it matches your model's input shapes)
-        const x_input = new ort.Tensor("float32", new Float32Array(6 * 10).fill(1.0), [1, 6, 10]); // Batch size 1
-        const d_input = new ort.Tensor("float32", new Float32Array([0.5]), [1, 1]); // Batch size 1
-
+        
         const gk_data = await fetch("https://deangumas.github.io/data/GK_2021-22.json");
-        const sample = gk_data["David de Gea"]
-        const feeds = { "player_data": sample["player_data"], "team_rating": sample["team_rating"][0] };
+        const real_data = await gk_data.json();
+        const keys = Object.keys(real_data);
+        const randomKey = keys[Math.floor(Math.random() * keys.length)];
+        const sample = real_data[randomKey];
+        const playerData = sample["player_data"]
+        
+        const x_input = new ort.Tensor("float32", new Float32Array(playerData.flat()), [1, 6, 10]);
+        const d_input = new ort.Tensor("float32", new Float32Array([0.5]), [1, 1]);
+
+        const feeds = { "player_data": x_input, "team_rating": d_input };
         const results = await session.run(feeds);
 
         console.log(results["score_prediction"].data);
 
         // Display result
         document.getElementById("output").innerText = `Model Output: ${results["score_prediction"].data}`;
+        document.getElementById("real_output").innerText = `True Output: ${sample["actual_score"]}`;
     } catch (error) {
         console.error("Error running the model:", error);
     }
